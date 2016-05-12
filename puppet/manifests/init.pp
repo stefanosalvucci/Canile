5d824df20adb6c3d3234e61c7be5c0f60389fba7
Mattia Usai <mattia@localhost.localdomain>
2016-05-12 17:30:05 +0200

    modified roles user and db password

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,3 +1,18 @@
 class { 'postgresql::server': }
 
+class { 'postgresql::server': }
 
+postgresql::server::db { 'canile_production':
+  user     => 'vagrant',
+  password => postgresql_password('vagrant', 'aswroma3'),
+}
+
+postgresql::server::role { 'vagrant':
+password_hash => postgresql_password('vagrant', 'aswroma3'),
+}
+
+postgresql::server::database_grant { 'canile_production':
+  privilege => 'ALL',
+  db        => 'canile_production',
+  role      => 'vagrant',
+}
